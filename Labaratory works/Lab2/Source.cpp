2d05d91e5d101c4db1d43cb5015412dedb8b256a
Nero <72101790+PishchDaniil@users.noreply.github.com>
2021-10-16 21:10:51 +0300

    Update Source.cpp

--- Labaratory works/Lab2/Source.cpp
+++ Labaratory works/Lab2/Source.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 #define endl cout << "\n";
 
-
 class Logic_function
 {
 public:
@@ -25,46 +24,6 @@ public:
 		}
 	}
 
-	void print_array_minimization(const string(&Array)[15][3])
-	{
-		for (int i = 0; i < 15; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				cout << Array[i][j];
-			}
-			endl
-		}
-		endl
-
-	}
-
-	void print_array_check(const int(&Array)[3][4])
-	{
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				cout << Array[i][j];
-			}
-			endl
-		}
-		endl
-	}
-
-	void print_array_check1(const int(&Array)[1][4])
-	{
-		for (int i = 0; i < 1; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				cout << Array[i][j];
-			}
-			endl
-		}
-		endl
-	}
-
 	static void print_deque(const deque<char>& dq)
 	{
 		for (auto i : dq)
@@ -82,9 +41,10 @@ public:
 		int j = -1, num = 0, count = 0;
 		bool num_have_in_column = false;
 		container.clear();
-		for (int i = 0; i < 5; i++)
+		int default_column_size = 16, default_strings_size = 5;
+		for (int i = 0; i < default_strings_size; i++)
 		{
-			for (int j = 0; j < 16; j++)
+			for (int j = 0; j < default_column_size; j++)
 			{
 				spreadsheet_truth[i][j] = false;
 			}
@@ -181,6 +141,8 @@ public:
 		preparing_function.clear();
 		numbr_of_column = 8;
 		bit_depth = 3;
+		int default_column_size = 16, default_strings_size = 5;
+		int bit_size_container = 8;
 
 		for (int i = 0; i < logical_fucntion.size(); i++)
 		{
@@ -259,9 +221,9 @@ public:
 
 		//print_deque(preparing_function);
 
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < default_strings_size; i++)
 		{
-			for (int j = 0; j < 16; j++)
+			for (int j = 0; j < default_column_size; j++)
 			{
 				spreadsheet_truth[i][j] = false;
 			}
@@ -297,7 +259,7 @@ public:
 
 
 		container_result = processing(preparing_function);
-		for (int i = 0; i < 8; i++)
+		for (int i = 0; i < bit_size_container; i++)
 		{
 			if (container_result[i] == 1) num_i += pow(2, 8 - (i + 1));
 			spreadsheet_truth[bit_depth][i] = container_result[i];
@@ -313,296 +275,6 @@ public:
 		endl
 			cout << "Index = " << num_i;
 		endl
-	}
-
-	void minimization_method_Cvain_Mak_Klaski() // !(X+Y)+!Z#   //in develop
-	{
-
-		int no_one_true[1][4], one_in_true[3][4], two_in_true[3][4], tree_in_true[1][4], trigger_for_c = false, trigger_for_s = false;
-		string firstX[15][3];
-		bool array_truth[10][10];
-		bool stringUp = false, stringUp1 = false, stringUp2 = false;
-
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				if (i < 1)
-				{
-					no_one_true[i][j] = 0;
-					tree_in_true[i][j] = 0;
-				}
-				one_in_true[i][j] = 0;
-				two_in_true[i][j] = 0;
-			}
-		}
-
-		for (int i = 0; i < 15; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				firstX[i][j] = "";
-			}
-		}
-
-		int number_of_input = 0;
-		int s = 0, c = 0;
-		for (int i = 0; i < numbr_of_column; i++)
-		{
-			number_of_input = 0;
-			if (spreadsheet_truth[3][i])
-			{
-
-				for (int j = 0; j < bit_depth; j++)
-				{
-					if (spreadsheet_truth[j][i]) number_of_input++;
-
-				}
-				trigger_for_c = false;
-				trigger_for_s = false;
-
-				for (int j = 0; j < this->bit_depth; j++)
-				{
-
-					if (number_of_input == 0)
-					{
-						no_one_true[0][j + 1] = spreadsheet_truth[j][i];
-						no_one_true[0][0] = number_of_input + 9;
-					}
-					else if (number_of_input == 1)
-					{
-						one_in_true[s][j + 1] = spreadsheet_truth[j][i];
-						trigger_for_s = true;
-						one_in_true[s][0] = number_of_input;
-					}
-					else if (number_of_input == 2)
-					{
-						two_in_true[c][j + 1] = spreadsheet_truth[j][i];
-						trigger_for_c = true;
-						two_in_true[c][0] = number_of_input;
-					}
-					else if (number_of_input == 3)
-					{
-						tree_in_true[0][j + 1] = spreadsheet_truth[j][i];
-						tree_in_true[0][0] = number_of_input;
-					}
-
-				}
-				if (trigger_for_s) s++;
-				if (trigger_for_c) c++;
-			}
-		}
-
-		print_array_check1(no_one_true);
-		print_array_check(one_in_true);
-		print_array_check(two_in_true);
-		print_array_check1(tree_in_true);
-		int mut = 0, mut1 = 0, mut2 = 0, str = 0;
-
-
-		mut = 0;
-		mut1 = 0;
-		mut2 = 0;
-
-		stringUp = false;
-		stringUp1 = false;
-		stringUp2 = false;
-
-		for (int j = 1; j < 4; j++)  //пропускаем число вхождений
-		{
-
-			if ((no_one_true[0][j] == 0 && one_in_true[0][j] == 1 || no_one_true[0][j] == 1 && one_in_true[0][j] == 0) && one_in_true[0][0] == 1 && no_one_true[0][0] == 9) mut++;
-			if ((no_one_true[0][j] == 0 && one_in_true[1][j] == 1 || no_one_true[0][j] == 1 && one_in_true[1][j] == 0) && one_in_true[1][0] == 1 && no_one_true[0][0] == 9) mut1++;
-			if ((no_one_true[0][j] == 0 && one_in_true[2][j] == 1 || no_one_true[0][j] == 1 && one_in_true[2][j] == 0) && one_in_true[2][0] == 1 && no_one_true[0][0] == 9) mut2++;
-		}
-
-		for (int q = 0; q < 3; q++)  //пропускаем число вхождений
-		{
-			if (mut == 1)
-			{
-				if (no_one_true[0][q + 1]) firstX[str][q] += '1';
-				else firstX[str][q] += '0';
-				stringUp = true;
-			}
-
-			if (mut == 1 && (no_one_true[0][q + 1] == 0 && one_in_true[0][q + 1] == 1 || no_one_true[0][q + 1] == 1 && one_in_true[0][q + 1] == 0))
-			{
-				firstX[str][q] += 'X';
-			}
-
-			if (mut1 == 1)
-			{
-				if (no_one_true[0][q + 1]) firstX[str + 1][q] = '1';
-				else firstX[str + 1][q] += '0';
-				stringUp1 = true;
-			}
-
-			if (mut1 == 1 && (no_one_true[0][q + 1] == 1 && one_in_true[1][q + 1] == 0 || no_one_true[0][q + 1] == 0 && one_in_true[1][q + 1] == 1))
-			{
-				firstX[str + 1][q] += 'X';
-			}
-
-			if (mut2 == 1)
-			{
-				if (no_one_true[0][q + 1]) firstX[str + 2][q] = '1';
-				else firstX[str + 2][q] += '0';
-				stringUp2 = true;
-			}
-
-			if (mut2 == 1 && (no_one_true[0][q + 1] == 0 && one_in_true[2][q + 1] == 1 || no_one_true[0][q + 1] == 1 && one_in_true[2][q + 1] == 0))
-			{
-				firstX[str + 2][q] += 'X';
-			}
-
-		}
-
-		if (stringUp || stringUp1 || stringUp2)
-		{
-			str += (int)stringUp + (int)stringUp1 + (int)stringUp2;
-		}
-
-
-		//using x___  _x___
-		//сранвивем 2 массива определяем __X_ и т.д надо подумать
-		str++;
-		for (int i = 0; i < 3; i++) //сравниваю слеива.тбся ли только смежные т.е 1-1 2-2 но не промежуточные надо пофиксить
-		{
-			mut = 0;
-			mut1 = 0;
-			mut2 = 0;
-
-			stringUp = false;
-			stringUp1 = false;
-			stringUp2 = false;
-
-			for (int j = 1; j < 4; j++)  //пропускаем число вхождений
-			{
-
-				if ((one_in_true[i][j] == 0 && two_in_true[0][j] == 1 || one_in_true[i][j] == 1 && two_in_true[0][j] == 0) && one_in_true[0][0] == 1 && two_in_true[0][0] == 2) mut++;
-				if ((one_in_true[i][j] == 0 && two_in_true[1][j] == 1 || one_in_true[i][j] == 1 && two_in_true[1][j] == 0) && one_in_true[1][0] == 1 && two_in_true[1][0] == 2) mut1++;
-				if ((one_in_true[i][j] == 0 && two_in_true[2][j] == 1 || one_in_true[i][j] == 1 && two_in_true[2][j] == 0) && one_in_true[2][0] == 1 && two_in_true[2][0] == 2) mut2++;
-			}
-
-			for (int q = 0; q < 3; q++)  //пропускаем число вхождений
-			{
-				if (mut == 1)
-				{
-					if (one_in_true[i][q + 1]) firstX[str][q] = '1';
-					else firstX[str][q] = '0';
-					stringUp = true;
-				}
-
-				if (mut == 1 && (one_in_true[i][q + 1] == 0 && two_in_true[0][q + 1] == 1 || mut == 1 && one_in_true[i][q + 1] == 1 && two_in_true[0][q + 1] == 0))
-				{
-					firstX[str][q] = 'X';
-				}
-
-				if (mut1 == 1)
-				{
-					if (one_in_true[i][q + 1]) firstX[str + 1][q] = '1';
-					else firstX[str + 1][q] = '0';
-					stringUp1 = true;
-				}
-
-				if (mut1 == 1 && (one_in_true[i][q + 1] == 1 && two_in_true[1][q + 1] == 0 || one_in_true[i][q + 1] == 0 && two_in_true[1][q + 1] == 1))
-				{
-					firstX[str + 1][q] = 'X';
-				}
-
-				if (mut2 == 1)
-				{
-					if (one_in_true[i][q + 1]) firstX[str + 2][q] = '1';
-					else firstX[str + 2][q] = '0';
-					stringUp2 = true;
-				}
-
-				if (mut2 == 1 && (one_in_true[i][q + 1] == 0 && two_in_true[2][q + 1] == 1 || one_in_true[i][q + 1] == 1 && two_in_true[2][q + 1] == 0))
-				{
-					firstX[str + 2][q] = 'X';
-				}
-
-			}
-			if (stringUp || stringUp1 || stringUp2)
-			{
-				str += (int)stringUp + (int)stringUp1 + (int)stringUp2;
-			}
-		}
-		// with 3 mass
-		str++;
-		mut = 0;
-		mut1 = 0;
-		mut2 = 0;
-
-		stringUp = false;
-		stringUp1 = false;
-		stringUp2 = false;
-
-		for (int j = 1; j < 4; j++)  //пропускаем число вхождений
-		{
-
-			if (((two_in_true[0][j] == 0 && tree_in_true[0][j] == 1 || two_in_true[0][j] == 1 && tree_in_true[0][j] == 0) && two_in_true[0][0] == 2 && tree_in_true[0][0] == 3)) mut++;
-			if (((two_in_true[1][j] == 0 && tree_in_true[0][j] == 1 || two_in_true[1][j] == 1 && tree_in_true[0][j] == 0) && two_in_true[1][0] == 2 && tree_in_true[0][0] == 3)) mut1++;
-			if (((two_in_true[2][j] == 0 && tree_in_true[0][j] == 1 || two_in_true[2][j] == 1 && tree_in_true[0][j] == 0) && two_in_true[2][0] == 2 && tree_in_true[0][0] == 3)) mut2++;
-		}
-
-		for (int q = 0; q < 3; q++)  //пропускаем число вхождений
-		{
-			if (mut == 1)
-			{
-				if (two_in_true[0][q + 1]) firstX[str][q] = '1';
-				else firstX[str][q] = '0';
-				stringUp = true;
-			}
-
-			if (mut == 1 && (two_in_true[0][q + 1] == 0 && tree_in_true[0][q + 1] == 1 || two_in_true[0][q + 1] == 1 && tree_in_true[0][q + 1] == 0))
-			{
-				firstX[str][q] = 'X';
-			}
-
-			if (mut1 == 1)
-			{
-				if (two_in_true[1][q + 1]) firstX[str + 1][q] = '1';
-				else firstX[str + 1][q] = '0';
-				stringUp1 = true;
-			}
-
-			if (mut1 == 1 && (two_in_true[1][q + 1] == 0 && tree_in_true[0][q + 1] == 1 || two_in_true[1][q + 1] == 1 && tree_in_true[0][q + 1] == 0))
-			{
-				firstX[str + 1][q] = 'X';
-			}
-
-			if (mut2 == 1)
-			{
-				if (two_in_true[2][q + 1]) firstX[str + 2][q] = '1';
-				else firstX[str + 2][q] = '0';
-				stringUp2 = true;
-			}
-
-			if (mut2 == 1 && (two_in_true[2][q + 1] == 0 && tree_in_true[0][q + 1] == 1 || two_in_true[2][q + 1] == 1 && tree_in_true[0][q + 1] == 0))
-			{
-				firstX[str + 2][q] = 'X';
-			}
-
-		}
-
-		//таблица 
-		bool cover_number = false;
-		/*int j = 0;
-		for (int i = 0; i < 10; i++)
-		{
-				cover_number = false;
-				for (int q = 0 ; q < 3; q++)
-				{
-					if (one_in_true[i][q + 1] == 1 && firstX[i][q] == "1" || one_in_true[i][q + 1] == 0 && firstX[i][q] == "0" || firstX[i][j] == "X") cover_number = true;
-				}
-				cover_number = false;
-				if (cover_number) array_truth[i][j] = true;
-
-		}
-		*/
-
-		print_array_minimization(firstX); //отлично, вроде работает, теперь таблица покрытия
-
 	}
 
 	void SDNF_and_SKNF()
@@ -807,7 +479,7 @@ public:
 	string get_SKNF_rez() { return SKNF_rez; }
 
 private:
-	bool spreadsheet_truth[5][16];
+	
 	int bit_depth = 0;
 	int numbr_of_column = 0;
 	int number_of_terms_SDNF = 0;
@@ -816,13 +488,19 @@ private:
 	string SKNF_rez = "";
 	string TDNF_rez = "";
 	string TKNF_rez = "";
+	static const int number_of_strings = 5;
+	static const int number_of_columns = 16;
+	bool spreadsheet_truth[number_of_strings][number_of_columns];
+
 };
 
 void main()
 {
 	deque<char> analyzing_function;
-	int choose;
+	int choose, max_number_input = 256, numbr_of_input = 100;
 	deque<int> function;
+	static const int null = 0;
+	static const int max_number_input_user = 15;
 	bool reset = true, checker = false, second_task = false;
 	Logic_function exmpl;
 	while (reset)
@@ -848,32 +526,44 @@ void main()
 		}
 		case 1:
 		{
-			checker = false;
-
-			int num = 0;
-			if (second_task)
+			try
 			{
-				checker = true;
-				cout << "Input i function: ";
-				cin >> num;
-			}
-			else
-			{
-				function.clear();
-				int num, size = 0;
-				cout << "Input size function: ";
-				cin >> size;
-				cout << "Input function: ";
-
-				for (int i = 0; i < size; i++)
+				checker = false;
+				int j = 0;
+				int num = 0;
+				if (second_task)
 				{
+					checker = true;
+					cout << "Input i function: ";
 					cin >> num;
-					function.push_back(num);
+					if (num >= max_number_input_user) throw 2;
+
+				}
+				else
+				{
+					function.clear();
+					int num = 0, size = 0;
+					cout << "Input size function: ";
+					cin >> size;
+					if (!(size >= null && size <= max_number_input)) throw 2;
+					cout << "Input function: ";
+
+					for (int i = 0; i < size; i++)
+					{
+						cin >> num;
+						function.push_back(num);
+						if (!(num >= null && num <= max_number_input)) throw 2;
+					}
+
 				}
 
+				exmpl.analysis_input(function, checker, num);
 			}
-			exmpl.analysis_input(function, checker, num);
 
+			catch (int error)
+			{
+				if (error == 2) cout << "Uncorrect unput!\n";
+			}
 			break;
 		}
 		case 3:
@@ -883,21 +573,22 @@ void main()
 				char symbol = 0;
 				int number_of_breckets = 0;
 				cout << "Input function, for stopping input tape (#): ";
-				for (int i = 0; i < 100; i++)
+				for (int i = 0; i < numbr_of_input; i++)
 				{
 					cin >> symbol;
 					if (symbol == '#') break;
 					if (symbol == '(') number_of_breckets++;
-					if (symbol != 'X' || symbol != 'Y' || symbol != 'Z' || symbol != '(' || symbol != ')' || symbol != '*' || symbol != '+' || symbol != '!') throw 1;
+					if (symbol != 'X' && symbol != 'Y' && symbol != 'Z' && symbol != '(' && symbol != ')' && symbol != '*' && symbol != '+' && symbol != '!') throw 1;
 					else if (symbol == ')') number_of_breckets--;
+					
 					analyzing_function.push_back(symbol);
 				}
 				if (number_of_breckets != 0) throw 1;
 				exmpl.analysis_input_logical_form(analyzing_function);
 			}
-			catch (int i)
+			catch (int error)
 			{
-				if (i == 1) cout << "Uncorrect unput!\n";
+				if (error == 1) cout << "Uncorrect unput!\n";
 			}
 			break;
 		}
